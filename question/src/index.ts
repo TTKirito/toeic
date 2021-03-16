546807c172cfa4841b1f733efca5daf3a838a433
thuan <thuanton98@gmail.com>
2021-03-16 08:40:04 +0700

    adf

--- question/src/index.ts
+++ question/src/index.ts
@@ -22,7 +22,6 @@ const start = async () => {
     }
 
     try {
-        console.log('dev')
         await natsWrapper.connect(
             process.env.NATS_CLUSTER_ID,
             process.env.NATS_CLIENT_ID,
